1ae3a6871339726734c49101108bdf92a3315de1
pinheirofrederico-beep <pinheiro.frederico@escola.pr.gov.br>
2026-05-06 08:49:13 -0300

    Update db.php

--- db.php
+++ db.php
@@ -1,1 +1,12 @@
+<?php
+$host = "localhost";
+$user = "root";
+$pass = "";
+$dbname = "sistema_usuarios";
 
+$conn = new mysqli($host, $user, $pass, $dbname);
+
+if ($conn->connect_error) {
+    die("Erro de conexão: " . $conn->connect_error);
+}
+?>
